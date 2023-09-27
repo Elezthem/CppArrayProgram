8d72e6cff921bbd0e7c874c496cb62aa129f21fd
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-27 06:46:01 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	
+	float nums[4];
+	for (int i = 0; i < 4; i++) {
+		cout << "Enter num " << i << ": ";
+		cin >> nums[i];
+	}
+
+	float sum = 0;
+	float min = nums[0];
+	for (int i = 0; i < 4; i++) {
+		sum += nums[i];
+		if (nums[i] < min)
+			min = nums[i];
+	}
+	cout << "Sum: " << sum << endl;
+	cout << "Min: " << min << endl;
+
+	for (int i = 0; i < 4; i++) {
+		cout << "Element: " << nums[i] << endl;
+	}
+
+	return 0;
+}
